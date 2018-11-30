ea04a5a3c2e0c6e033ede4001776bab26f7c9bd2
apetecuquis <45441797+apetecuquis@users.noreply.github.com>
2018-11-29 22:19:54 -0600

    cambio nombre

--- login/index.php
+++ login/index.php
@@ -33,7 +33,7 @@ if(isset($_SESSION['session']))
 					<!--ejemplo <label for="usuario">Usuario:</label>-->
 					<!--required es nuevo en html5, si el campo está vacío te avisa, pero cuidado, no valida la información-->
 					<p><label >Usuario:</label></p>
-						<input name="usuario" type="text" id="usuario" placeholder="Ingresa Usuario" autofocus="" required=""></p>
+						<input name="cliente" type="text" id="cliente" placeholder="Ingresa cliente" autofocus="" required=""></p>
 					
 					<p><label>Contraseña:</label></p>
 						<input name="contrasenia" type="password" id="contrasenia" placeholder="Ingresa Password" required=""></p>
