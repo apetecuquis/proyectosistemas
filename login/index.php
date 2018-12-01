0347dc1b6ca7bfde588f8182b472738c5ec941ef
apetecuquis <45441797+apetecuquis@users.noreply.github.com>
2018-11-30 23:25:38 -0600

    cambio titulo

--- login/index.php
+++ login/index.php
@@ -9,7 +9,7 @@ if(isset($_SESSIONES['session']))
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<title>INFTEC</title>
+	<title>SHOPTHEM</title>
 	<meta charset="utf-8">
 	<link type="text/css" href="./css/style.css" rel="stylesheet" />
 </head>
