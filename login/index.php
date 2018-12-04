83b39158d52bf80603b439d60678deb385a90a1d
apetecuquis <45441797+apetecuquis@users.noreply.github.com>
2018-12-03 19:29:00 -0600

    cambio usuarios

--- login/index.php
+++ login/index.php
@@ -32,7 +32,7 @@ if(isset($_SESSIONES['session']))
 					<!--A saber, el atributo for funciona como el id.-->
 					<!--ejemplo <label for="usuario">Usuario:</label>-->
 					<!--required es nuevo en html5, si el campo está vacío te avisa, pero cuidado, no valida la información-->
-					<p><label >Usuarios:</label></p>
+					<p><label >Clientes:</label></p>
 						<input name="cliente" type="text" id="cliente" placeholder="Ingresa cliente" autofocus="" required=""></p>
 					
 					<p><label>Contraseñas:</label></p>
